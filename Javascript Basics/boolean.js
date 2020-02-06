772d27e7cfefe605b92078c490612ad20ba693d5
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-02-06 16:18:43 +0100

    booleans getest

---
+++ Javascript Basics/boolean.js
@@ -0,0 +1,24 @@
+//  soorten operators
+//  === - gelijk  aan
+//  !== - niet gelijk aan 
+//  < - kleiner dan 
+//  > - groter dan
+//  <=  kleiner of gelijk aan
+//  >= groter of gelijk aan
+
+let temp = 0;
+let hetVriest = temp <= 0;
+
+console.log(hetVriest);
+
+//  de uitdaging
+
+// mijn leeftijd
+let leeftijd = 24;
+
+// leeftijdscheck
+let isKind = leeftijd <= 7;
+let isVolwassen = leeftijd >= 18;
+
+console.log(isKind);
+console.log(isVolwassen);
