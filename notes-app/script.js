65000f7b28c25e89aca64127e30c1919100e77b2
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-02 14:42:23 +0100

    todo-app aangemaakt

--- notes-app/script.js
+++ notes-app/script.js
@@ -2,11 +2,11 @@
 // const p = document.querySelector('p')
 // p.remove()
 
-query all and remove
+// query all and remove
 const ps = document.querySelectorAll('p')
 
-ps..forEach(function(p) {
-    p.textContent = '******'
-        // console.log(p.textContent)
-        // p.remove()
+ps.forEach(function(p) {
+    p.textContent = '******';
+    // console.log(p.textContent)
+    // p.remove()
 });
