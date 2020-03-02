f31a05ea5591583c70adf48c1ceaf1edbf76a496
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-02 15:19:40 +0100

    begonnen met addEventListener

--- notes-app/script.js
+++ notes-app/script.js
@@ -9,23 +9,6 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-// query and remove
-// const p = document.querySelector('p')
-// p.remove()
-
-// query all and remove
-const ps = document.querySelectorAll('p')
-
-ps.forEach(function(p) {
-    p.textContent = '******';
-    // console.log(p.textContent)
-    // p.remove()
-});
-
-// Challenge area
-// Add a new element
-const nieuwPara = document.createElement('p')
-
-nieuwPara.textContent = 'Dit is een nieuwe p vanuit js '
-
-document.querySelector('main').appendChild(nieuwPara)
+document.querySelector('button').addEventListener('click', function(e) {
+    e.target.textContent = 'De button is al gebruikt'
+})
--- todo-app/script.js
+++ todo-app/script.js
@@ -33,3 +33,8 @@ todos.forEach(function(todo) {
     p.textContent = todo.text
     document.querySelector('body').appendChild(p)
 });
+
+document.querySelector('button').addEventListener('click', function(e) {
+
+
+})
