42975795f3a828dc7bf34d9291796300bd8a5930
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-02 14:13:59 +0100

    filteren bij arrays geprobeerd

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -10,16 +10,30 @@ const notes = [{
 }]
 
 const findNote = function(notes, noteTitel) {
-        return notes.find(function(note, index) {
-            return note.titel.toLowerCase() === noteTitel.toLowerCase()
-        })
-    }
-    // const findNote = function(notes, noteTitel) {
-    //     const index = notes.findIndex(function(note, index) {
-    //         return note.titel.toLowerCase() === noteTitel.toLowerCase()
-    //     })
-    //     return notes[index]
-    // }
+    return notes.find(function(note, index) {
+        return note.titel.toLowerCase() === noteTitel.toLowerCase()
+    })
+}
+
+const findNotes = function(notes, query) {
+    return notes.filter(function(note, index) {
+        const isTitleMatch = note.titel.toLowerCase().includes(query.toLowerCase())
+        const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
+        return isTitleMatch || isBodyMatch
+    })
+}
+
+console.log(findNotes(notes, 'en'));
+
+
+
+
+// const findNote = function(notes, noteTitel) {
+//     const index = notes.findIndex(function(note, index) {
+//         return note.titel.toLowerCase() === noteTitel.toLowerCase()
+//     })
+//     return notes[index]
+// }
 
 const note = findNote(notes, 'verbeteringen')
 console.log(note);
