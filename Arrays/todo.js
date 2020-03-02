294b057423c011daa4ccbfeb3cca6b13aa8a7056
RodneydeBoer <rodneydeboer1@hotmail.com>
2020-03-02 12:31:20 +0100

    sectie 6: opdracht 39

---
+++ Arrays/todo.js
@@ -0,0 +1,11 @@
+// challenge area
+
+// create an array with 5 to-do's
+
+const todo = ['boodschappen doen', 'de was', 'stofzuigen', 'dweilen', 'vuilnisbak']
+    // you have x todos (length)
+    // print the first and second to last items > todo: walk the dog (example)
+
+console.log(`You have ${todo.length} things to do`);
+console.log(`You have to do ${todo[0]} first`);
+console.log(`You have to do ${todo[todo.length - 2]} as second to last`);
